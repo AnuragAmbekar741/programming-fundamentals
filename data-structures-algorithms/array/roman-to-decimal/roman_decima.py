a2d2bb8173352e258ea74fa1706ba474df49a5e9
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-04 17:30:55 -0500

    roman to decimal

---
+++ data-structures-algorithms/array/roman-to-decimal/roman_decima.py
@@ -0,0 +1,18 @@
+def roman_to_decimal(roman:str)->int:
+    roman_hash_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    
+    for i in range(len(roman)):
+        if i+1<len(roman) and roman_hash_map[roman[i]]<roman_hash_map[roman[i+1]]:
+            result-=roman_hash_map[roman[i]]
+        else:
+            result+=roman_hash_map[roman[i]]
+    return result
