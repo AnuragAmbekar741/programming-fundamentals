3950d03d2581ca9648cc937c526dc5a1c1175622
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-04 10:56:34 -0500

    top k freq eles

--- data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
+++ data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
@@ -1,4 +1,4 @@
-def top_k_freq_ele(nums:list[int],k:int)->int or bool:
+def top_k_freq_ele(nums:list[int],k:int)->list[int]:
     num_hash_map = {}
     result = []
     for num in nums:
@@ -18,3 +18,17 @@ def top_k_freq_ele(nums:list[int],k:int)->int or bool:
                     return result
     return result
 print(top_k_freq_ele([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4],2))
+
+
+def top_k_freq_ele2(nums:list[int],k:int)->list[int]:
+    num_hash_map = {}
+    result = []
+    for num in nums:
+        num_hash_map[num] = num_hash_map.get(num,0)+1
+    for num, freq in num_hash_map.items():
+        if freq >= k:
+            result.append(num)
+
+    return result[:k] if len(result) >= k else result
+print(top_k_freq_ele2([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4],2))
+
