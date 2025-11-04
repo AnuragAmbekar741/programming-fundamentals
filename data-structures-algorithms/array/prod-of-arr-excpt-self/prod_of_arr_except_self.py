2897aaae8a4fc120a88480c94b0d8470a75dfff4
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-03 23:59:41 -0500

    prod of arr except self

--- data-structures-algorithms/array/prod-of-arr-excpt-self/prod_of_arr_except_self.py
+++ data-structures-algorithms/array/prod-of-arr-excpt-self/prod_of_arr_except_self.py
@@ -10,3 +10,18 @@ def prod_of_arr_except_self(nums:list[int])->list[int]:
     return result
 
 print(prod_of_arr_except_self([1,2,3,4]))
+
+
+def prod_of_arr_except_self2(nums:list[int])->list[int]:
+    n = len(nums)
+    result = [1]*n
+    print(result)
+    for i in range(1,n):
+        result[i] = result[i-1]*nums[i-1]
+    suffix = 1
+    for i in range(n-1, -1, -1):  # Go backwards
+        result[i] *= suffix       # Multiply prefix * suffix
+        suffix *= nums[i]  
+    return result
+
+print(prod_of_arr_except_self2([1,2,3,4]))
