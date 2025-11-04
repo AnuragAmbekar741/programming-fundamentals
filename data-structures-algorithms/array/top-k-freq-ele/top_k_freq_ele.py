225927b39c1178b6e28f87a03d4795be6e437611
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-04 11:08:58 -0500

    top k freq eles

--- data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
+++ data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
@@ -1,34 +1,11 @@
-def top_k_freq_ele(nums:list[int],k:int)->list[int]:
-    num_hash_map = {}
-    result = []
-    for num in nums:
-        if num in num_hash_map:
-            num_hash_map[num] += 1
-            if num_hash_map[num] >= k:
-                if(len(result) < k):
-                    result.append(num)
-                else:
-                    return result
-        else:
-            num_hash_map[num] = 1
-            if num_hash_map[num] >= k:
-                if(len(result) < k):
-                    result.append(num)
-                else:
-                    return result
-    return result
-print(top_k_freq_ele([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4],2))
-
-
 def top_k_freq_ele2(nums:list[int],k:int)->list[int]:
     num_hash_map = {}
     result = []
     for num in nums:
         num_hash_map[num] = num_hash_map.get(num,0)+1
-    for num, freq in num_hash_map.items():
-        if freq >= k:
-            result.append(num)
+    sorted_num_freq = sorted(num_hash_map.items(),key=lambda x:x[1],reverse=True)
+    return [item[0] for item in sorted_num_freq[:k]]
 
     return result[:k] if len(result) >= k else result
-print(top_k_freq_ele2([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4],2))
+print(top_k_freq_ele2([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4,4,4,4,4,2],2))
 
