bb0778629d912c960af9957a96dc31f6361acc7b
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-05 12:07:16 -0500

    resvised roman to decimal

--- data-structures-algorithms/array/roman-to-decimal/roman_decima.py
+++ data-structures-algorithms/array/roman-to-decimal/roman_decima.py
@@ -1,27 +1,4 @@
-def roman_to_decimal(roman:str)->int:
-    roman_hash_map = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    result = 0
-
-    for i in range(len(roman)):
-        if i+1<len(roman) and roman_hash_map[roman[i]]<roman_hash_map[roman[i+1]]:
-            result-=roman_hash_map[roman[i]]
-        else:
-            result+=roman_hash_map[roman[i]]
-    return result
-
-print(roman_to_decimal("XIX"))
-
-
-def roman_to_decimal2(roman:str)->int:
-    roman_hash_map = {
+roman_hash_map = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -30,7 +7,7 @@ def roman_to_decimal2(roman:str)->int:
         'D': 500,
         'M': 1000,
     }
-    compound_values = {
+compound_values = {
         'IV': 4,
         'IX': 9,
         'XL': 40,
@@ -38,6 +15,23 @@ def roman_to_decimal2(roman:str)->int:
         'CD': 400,
         'CM': 900,
     }
+
+
+def roman_to_decimal(roman:str)->int:
+    result = 0
+
+    for i in range(len(roman)):
+        if i + 1 < len(roman) and roman_hash_map[roman[i]] < roman_hash_map[roman[i+1]]:
+            result -= roman_hash_map[roman[i]]
+        else:
+            result += roman_hash_map[roman[i]]
+
+    return result
+
+print("1",roman_to_decimal("XIX"))
+
+
+def roman_to_decimal2(roman:str)->int:
     result = 0
     skip_next = False
     for i in range(len(roman)):
@@ -51,32 +45,18 @@ def roman_to_decimal2(roman:str)->int:
             result += roman_hash_map[roman[i]]
     return result
 
-print(roman_to_decimal2("XIX"))
+print("2",roman_to_decimal2("XIX"))
 
 def roman_to_decimal3(roman:str)->int:
-    roman_hash_map = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
-    }
-    compound_values = {
-        'IV': 4,
-        'IX': 9,
-        'XL': 40,
-        'XC': 90,
-        'CD': 400,
-        'CM': 900,
-    }
     result = 0
     i=0
     while i < len(roman):
         if i + 1 < len(roman) and roman[i:i+2] in compound_values:
-            result+=compound_values[roman[i:i+2]]
+            result+= compound_values[roman[i:i+2]]
+            i+=2
         else:
-            result+=roman_hash_map[roman[i]] 
+            result+=roman_hash_map[roman[i]]
+            i+=1
+    return result
 
-print(roman_to_decimal3("XIX"))
+print("3",roman_to_decimal3("XLIX"))
