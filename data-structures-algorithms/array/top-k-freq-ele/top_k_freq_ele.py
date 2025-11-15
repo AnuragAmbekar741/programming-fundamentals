5c4e43398d25e6fad463a590def4beea4fc702d9
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-11-14 23:24:48 -0500

    top - k element base sol

--- data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
+++ data-structures-algorithms/array/top-k-freq-ele/top_k_freq_ele.py
@@ -1,11 +1,11 @@
-def top_k_freq_ele2(nums:list[int],k:int)->list[int]:
-    num_hash_map = {}
-    result = []
+def topKFrequent(nums: list[int], k: int) -> list[int]:
+    result = {}
     for num in nums:
-        num_hash_map[num] = num_hash_map.get(num,0)+1
-    sorted_num_freq = sorted(num_hash_map.items(),key=lambda x:x[1],reverse=True)
-    return [item[0] for item in sorted_num_freq[:k]]
+        if num in result:
+            result[num]+=1
+        else:
+            result[num]=1
+    sorted_keys = sorted(result.keys(), key=lambda k: result[k])
+    return sorted_keys[-k:]
 
-    return result[:k] if len(result) >= k else result
-print(top_k_freq_ele2([1,2,2,3,1,2,1,4,5,3,6,7,7,7,4,4,4,4,4,4,2],2))
-
+print(topKFrequent([1,22,3,4,5,3,1,3],3))
