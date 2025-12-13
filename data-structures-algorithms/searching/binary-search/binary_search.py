cb3910bf8991a270c48323cf6e228e3ae0e20e60
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-12-12 23:13:53 -0500

    binary search in python

---
+++ data-structures-algorithms/searching/binary-search/binary_search.py
@@ -0,0 +1,17 @@
+import math
+
+
+def binary_search(arr:list[int],target:int)->int:
+    low = 0
+    high = len(arr)
+    while low <= high:
+        mid = math.floor(low + (high-low) // 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+    return -1
+
+print(binary_search([1,2,3,4,5,6,7,8,9,10], 8))
