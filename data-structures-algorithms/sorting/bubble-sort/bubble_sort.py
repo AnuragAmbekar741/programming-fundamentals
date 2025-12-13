788c5262cce40a2edd9d7b4be4429785b83791b3
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-12-12 23:02:00 -0500

    buble sort in python

---
+++ data-structures-algorithms/sorting/bubble-sort/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(arr:list[int])->list[int]:
+    for i in range(len(arr)):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+print(bubble_sort([5,3,8,4,2])) 
