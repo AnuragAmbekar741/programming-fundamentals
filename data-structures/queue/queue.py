7d0d79e9051dd126e79c5532211620c42454f64e
AnuragAmbekar741 <anuragambekar1997@gmail.com>
2025-12-22 13:00:28 -0500

    2. queue implementation in TS final

--- data-structures/queue/queue.py
+++ data-structures/queue/queue.py
@@ -32,3 +32,44 @@ class Queue(Generic[T]):
     
     def peek(self)->Optional[T]:
         return self.head.value if self.head else None
+
+
+
+# type Node<T> = {
+#   value: T;
+#   next?: Node<T> | null;
+# };
+
+# export default class Queue<T> {
+#   public length: number;
+#   private head?: Node<T>;
+#   private tail?: Node<T>;
+#   constructor() {
+#     this.head = this.tail = undefined;
+#     this.length = 0;
+#   }
+
+#   enqueue(item: T): void {
+#     const node = { value: item } as Node<T>;
+#     this.length++;
+#     if (!this?.tail) {
+#       this.tail = this.head = node;
+#       return;
+#     }
+#     this.tail.next = node;
+#     this.tail = node;
+#   }
+
+#   dequeue(): T | undefined {
+#     if (!this.head) {
+#       return undefined;
+#     }
+#     this.length--;
+#     const head = this.head;
+#     this.head = this.head.next ?? undefined;
+#     return head.value;
+#   }
+#   peek(): T | undefined {
+#     return this.head?.value;
+#   }
+# }
--- data-structures/queue/queue.ts
+++ data-structures/queue/queue.ts
@@ -1,37 +1,33 @@
-type Node<T> = {
+type QNode<T> = {
   value: T;
-  next?: Node<T> | null;
+  next?: QNode<T>;
 };
 
 export default class Queue<T> {
   public length: number;
-  private head?: Node<T>;
-  private tail?: Node<T>;
+  private head?: QNode<T>;
+  private tail?: QNode<T>;
   constructor() {
     this.head = this.tail = undefined;
     this.length = 0;
   }
 
   enqueue(item: T): void {
-    const node = { value: item } as Node<T>;
-    this.length++;
-    if (!this?.tail) {
-      this.tail = this.head = node;
-      return;
-    }
+    const node = { value: item } as QNode<T>;
+    if (!this.tail) this.head = this.tail = node;
+    this.length += 1;
     this.tail.next = node;
     this.tail = node;
   }
 
   dequeue(): T | undefined {
-    if (!this.head) {
-      return undefined;
-    }
-    this.length--;
+    if (!this.head) return undefined;
+    this.length -= 1;
     const head = this.head;
     this.head = this.head.next ?? undefined;
     return head.value;
   }
+
   peek(): T | undefined {
     return this.head?.value;
   }
